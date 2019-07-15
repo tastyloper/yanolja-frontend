7ababa7ad30b5ceb961664809d7b7ae45b4ae4b5
tastyloper <tastyloper@gmail.com>
2019-07-15 18:11:28 +0900

    make footer component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -15,6 +17,7 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    SharedModule,
     AuthRoutingModule
   ]
 })
