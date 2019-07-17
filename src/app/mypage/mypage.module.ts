43c6a1742773d40559ce8b0da643e45b84ff2411
gayoungaa91 <gayoungaa91@gmail.com>
2019-07-17 10:22:46 +0900

    temp commit

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -7,15 +7,13 @@ import { MypageComponent } from './mypage/mypage.component';
 import { ReservationListComponent } from './reservation-list/reservation-list.component';
 import { ReservationDetailComponent } from './reservation-detail/reservation-detail.component';
 import { ReservationCancelComponent } from './reservation-cancel/reservation-cancel.component';
-import { MemberUpdateComponent } from './member-update/member-update.component';
 
 @NgModule({
   declarations: [
     MypageComponent,
     ReservationListComponent,
     ReservationDetailComponent,
-    ReservationCancelComponent,
-    MemberUpdateComponent
+    ReservationCancelComponent
   ],
   imports: [
     CommonModule,
