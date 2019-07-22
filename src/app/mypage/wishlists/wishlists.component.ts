8c1aa730d9fdba90ddf8a977d357c7616006bca5
tastyloper <tastyloper@gmail.com>
2019-07-22 15:38:55 +0900

    make wishlist

--- src/app/mypage/wishlists/wishlists.component.ts
+++ src/app/mypage/wishlists/wishlists.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SubTitleService } from '../../core/services/sub-title.service';
+
 @Component({
   selector: 'app-wishlists',
   templateUrl: './wishlists.component.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private subTitleService: SubTitleService) {}
 
   ngOnInit() {
+    this.subTitleService.pagaTitle = '찜목록';
+    this.subTitleService.pagaDescription = '찜한 숙소를 한 눈에!';
   }
-
 }
