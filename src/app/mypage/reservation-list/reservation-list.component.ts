c2c077476de4cdaebf8db9ac10834968e2802aa3
chonnomvv <chonnomvv@gmail.com>
2019-07-26 18:23:00 +0900

    edit: css

--- src/app/mypage/reservation-list/reservation-list.component.ts
+++ src/app/mypage/reservation-list/reservation-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SubTitleService } from '../../core/services/sub-title.service';
+
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private subTitleService: SubTitleService) { }
 
   ngOnInit() {
+    this.subTitleService.pagaTitle = '예약내역';
+    this.subTitleService.pagaDescription = '내가 예약/사용한 목록';
   }
 
 }
