a4ef87422c8aa91de48d0d0b87a9e61b90c4560c
gayoungaa91 <gayoungaa91@gmail.com>
2019-07-26 13:04:32 +0900

    detail

--- src/app/mypage/reservation-detail/reservation-detail.component.ts
+++ src/app/mypage/reservation-detail/reservation-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
 import { SubTitleService } from '../../core/services/sub-title.service';
+
+import { CancellationPolicyComponent } from 'src/app/shared/cancellation-policy/cancellation-policy.component';
+
 
 @Component({
   selector: 'app-reservation-detail',
@@ -8,14 +13,21 @@ import { SubTitleService } from '../../core/services/sub-title.service';
   styleUrls: ['./reservation-detail.component.scss']
 })
 export class ReservationDetailComponent implements OnInit {
-
+  bsModalRef: BsModalRef;
+  isCheck: false;
+ 
   constructor(
-    private subTitleService: SubTitleService
+    private subTitleService: SubTitleService,
+    private modalService: BsModalService
   ) {}
   
   ngOnInit() {
     this.subTitleService.pagaTitle = '예약상세';
     this.subTitleService.pagaDescription = '내역을 확인해보세요!';
-    }
+  }
 
+  cancellationPolicyOpen() {
+    this.bsModalRef = this.modalService.show(CancellationPolicyComponent, { class: 'modal-lg' });
+  }
 }
+  
